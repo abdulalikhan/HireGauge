53dd3a4a2023ad11f0268e7923384361faa943be
Abdul Ali Khan <alikhan@cocashcreators.com>
2020-06-05 20:25:52 +0500

    Updated SQL tables
    Added some missing tables to the database

--- hiregauge.sql
+++ hiregauge.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 09, 2020 at 10:40 AM
+-- Generation Time: May 13, 2020 at 11:54 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.4
 
@@ -24,6 +24,52 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `membertype` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `membertype`, `email`, `password`) VALUES
+(1, 'Administrator', 'admin@localhost', '123456');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answers`
+--
+
+CREATE TABLE `answers` (
+  `id` int(11) NOT NULL,
+  `assessment_id` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  `candidate_id` int(11) NOT NULL,
+  `selected_option` varchar(255) NOT NULL,
+  `correct_option` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `assessments`
+--
+
+CREATE TABLE `assessments` (
+  `id` int(11) NOT NULL,
+  `opening_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `time_limit` int(11) NOT NULL,
+  `no_of_questions` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
 -- Table structure for table `candidates`
 --
 
@@ -35,18 +81,9 @@ CREATE TABLE `candidates` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `dob` varchar(255) NOT NULL,
-  `gender` varchar(255) NOT NULL
+  `gender` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `candidates`
---
-
-INSERT INTO `candidates` (`id`, `firstname`, `lastname`, `membertype`, `email`, `password`, `dob`, `gender`) VALUES
-(1, 'John', 'Doe', 'Candidate', 'johndoe@gmail.com', '123456', '3/10/2020', 'Male'),
-(2, 'Jane', 'Doe', 'Candidate', 'janedoe@gmail.com', '123456', '3/28/2020', 'Female');
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `employers`
@@ -64,19 +101,126 @@ CREATE TABLE `employers` (
   `jobtitle` varchar(255) NOT NULL,
   `companyname` varchar(255) NOT NULL,
   `phonenum` varchar(255) NOT NULL,
-  `employees` varchar(255) NOT NULL
+  `employees` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `employers`
+-- Table structure for table `jobapplications`
 --
 
-INSERT INTO `employers` (`id`, `firstname`, `lastname`, `membertype`, `email`, `password`, `dob`, `gender`, `jobtitle`, `companyname`, `phonenum`, `employees`) VALUES
-(1, 'Steve', 'Jobs', 'Employer', 'steve@apple.com', '123456', '2/24/1955', 'Male', 'Founder', 'Apple', '+123456789', '300+');
+CREATE TABLE `jobapplications` (
+  `id` int(11) NOT NULL,
+  `opening_id` int(11) NOT NULL,
+  `candidate_id` int(11) NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `assessment_taken` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `jobopenings`
+--
+
+CREATE TABLE `jobopenings` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `opendate` varchar(255) NOT NULL,
+  `closedate` varchar(255) NOT NULL,
+  `companyname` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `salary` double NOT NULL,
+  `jobtype` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `assessment_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `receiver` varchar(255) NOT NULL,
+  `sender` varchar(255) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `body` mediumtext NOT NULL,
+  `date` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `practicematerial`
+--
+
+CREATE TABLE `practicematerial` (
+  `id` int(11) NOT NULL,
+  `skillarea` varchar(255) NOT NULL,
+  `topic` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `practicematerial`
+--
+
+INSERT INTO `practicematerial` (`id`, `skillarea`, `topic`, `url`) VALUES
+(1, 'excel', 'Excel Formulae', 'https://www.udmercy.edu/about/its/help/files/excel-formulas.pdf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `id` int(11) NOT NULL,
+  `assessment_id` int(11) NOT NULL,
+  `question_text` varchar(255) NOT NULL,
+  `option1` varchar(255) NOT NULL,
+  `option2` varchar(255) NOT NULL,
+  `option3` varchar(255) NOT NULL,
+  `option4` varchar(255) NOT NULL,
+  `correct_option` varchar(255) NOT NULL,
+  `skilltag` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `results`
+--
+
+CREATE TABLE `results` (
+  `id` int(11) NOT NULL,
+  `assessment_id` int(11) NOT NULL,
+  `application_id` int(11) NOT NULL,
+  `candidate_id` int(11) NOT NULL,
+  `total_score` int(11) NOT NULL,
+  `score` int(11) NOT NULL,
+  `timelimit` double NOT NULL,
+  `timetaken` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `answers`
+--
+ALTER TABLE `answers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `assessments`
+--
+ALTER TABLE `assessments`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `candidates`
@@ -91,20 +235,110 @@ ALTER TABLE `employers`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `jobapplications`
+--
+ALTER TABLE `jobapplications`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jobopenings`
+--
+ALTER TABLE `jobopenings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `practicematerial`
+--
+ALTER TABLE `practicematerial`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `results`
+--
+ALTER TABLE `results`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `answers`
+--
+ALTER TABLE `answers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `assessments`
+--
+ALTER TABLE `assessments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- AUTO_INCREMENT for table `candidates`
 --
 ALTER TABLE `candidates`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- AUTO_INCREMENT for table `employers`
 --
 ALTER TABLE `employers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `jobapplications`
+--
+ALTER TABLE `jobapplications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `jobopenings`
+--
+ALTER TABLE `jobopenings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `practicematerial`
+--
+ALTER TABLE `practicematerial`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `results`
+--
+ALTER TABLE `results`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
